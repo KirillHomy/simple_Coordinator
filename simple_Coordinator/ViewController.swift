ff33b003ed6fbf0befa2e5e58849e98487e3f84b
Kirill Khomicevich <khomicevichkirillwork@icloud.com>
2023-06-14 19:47:43 +0300

    Initial Commit

---
+++ simple_Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_Coordinator
+//
+//  Created by Kirill Khomicevich on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
